nayana487/garnet

commit 9e23f738b50b3c009efbe5284919185ec0817916

group inherits from tree model

app/controllers/groups_controller.rb
@@ -6,9 +6,8 @@ class GroupsController < ApplicationController
 
   def admin_dashboard group
     @group = group
-    @students = @group.members(is_admin: false)
-    @memberships_in_group = @group.memberships
-    @users_in_group = @memberships_in_group.map{|membership|membership.user}
+    @students = @group.memberships.where(is_admin: false)
+    @users_in_group = @students.map{|membership|membership.user}
     @users_in_group_ids = @users_in_group.map{|user|user.id}
     @users = User.all
     @users_not_in_group = @users.where.not(id: @users_in_group_ids)
@@ -20,8 +19,8 @@ class GroupsController < ApplicationController
     @user = user
     @group = group
     @student = @user.memberships.find_by(group_id: @group.id)
-    @attendances = @student.get_subgroups("attendances")
-    @submissions = @student.get_subgroups("submissions")
+    @attendances = @student.descendants_attr("attendances")
+    @submissions = @student.descendants_attr("submissions")
     render "report_card"
   end
 

app/controllers/assignments_controller.rb
@@ -4,7 +4,7 @@ class AssignmentsController < ApplicationController
     @group = Group.find(params[:group_id])
     @user = current_user
     @assignment = Assignment.new
-    @assignments = @group.get_subgroups("assignments")
+    @assignments = @group.descendants_attr("assignments")
   end
 
   def show

app/controllers/students_controller.rb
@@ -4,9 +4,7 @@ class StudentsController < ApplicationController
     @author = Membership.find_by(user_id: current_user.id)
     @observation = @author.authored_observations.new
 
-    @user = User.find(params[:id])
-    @student = @group.members[@user.id]
-    @membership = @group.memberships.exists?(user_id: @user.id)
-    if @membership then @membership = @group.memberships.find_by(user_id: @user.id) end
+    @student = User.find(params[:id])
+    @membership = @group.memberships.find_by(user_id: @student.id)
   end
 end

app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
 
   def breadcrumbs(group, user = nil)
     output = [(link_to group.title, group_path(group))]
-    group.all_parents.each do |group|
+    group.ancestors.each do |group|
       output.unshift((link_to group.title, group_path(group)))
     end
     if user
@@ -16,7 +16,7 @@ module ApplicationHelper
 
   def subgroup_tree_html(group)
     output = "<li><a href='/groups/#{group.id}'>#{group.title}</a><ul>"
-    group.subgroups.each do |subgroup|
+    group.children.each do |subgroup|
       output += subgroup_tree_html(subgroup)
     end
     output += "</ul></li>"
@@ -60,5 +60,5 @@ module ApplicationHelper
   def profile_path user
     "/profile?user=#{user.username}"
   end
-  
+
 end

app/models/group.rb
@@ -1,88 +1,64 @@
-class Group < ActiveRecord::Base
+class Group < Tree
   has_many :events
   has_many :assignments
   has_many :memberships
   has_many :attendances, through: :events
   belongs_to :parent, class_name: "Group"
-  has_many :subgroups, class_name: "Group", foreign_key: "parent_id"
+  has_many :children, class_name: "Group", foreign_key: "parent_id"
+
+  validates :title, presence: true, format: {with: /[a-zA-Z0-9\-]+/, message: "Only letters, numbers, and hyphens are allowed."}
+  validate :has_parent_and_unique_name_among_siblings
+
+  def has_parent_and_unique_name_among_siblings
+    if !self.parent
+      if self.class.all.count > 0
+        errors[:base].push("Each Group has to have a parent group.")
+      end
+    else
+      sibling = self.parent.children.find_by(title: self.title)
+      if sibling && sibling.id != self.id
+        errors.add(:title, "must be unique among this group's siblings.")
+      end
+    end
+  end
 
   def self.named(group_name)
     Group.find_by(title: group_name)
   end
 
-  def self.bulk_create(groups, parent = nil)
-    groups.each do |key, subgroups|
-      group = self.create(title: key, parent_id: parent)
-      if subgroups.length > 0
-        self.bulk_create(subgroups, group.id)
-      end
-    end
+  def owners
+    self.ancestors_attr("memberships").select{|m| m.is_admin}.collect{|m| m.user}.uniq
   end
 
-  def all_parents(collection = nil)
-    collection = collection || []
-    if self.parent
-      collection.push(self.parent)
-      self.parent.all_parents(collection)
-    end
-    return collection
+  def admins
+    self.memberships.where(is_admin: true).collect{|m| m.user}
   end
 
-  def subgroup_tree
-    tree = self.as_json
-    subgroups = []
-    self.subgroups.each do |subgroup|
-      subgroups.push(subgroup.subgroup_tree)
-    end
-    tree[:subgroups] = subgroups
-    return tree
+  def nonadmins
+    self.memberships.where(is_admin: false).collect{|m| m.user}
   end
 
-  def subgroup_array(collection = nil)
-    collection = collection || []
-    self.subgroups.each do |subgroup|
-      collection.push(subgroup)
-      subgroup.subgroup_array(collection)
-    end
-    return collection
+  def subnonadmins
+    self.descendants_attr("memberships").select{|m| !m.is_admin}.collect{|m| m.user}.uniq
   end
 
-  def get_subgroups key
-    self_result = self.send(key)
-    if self_result.respond_to? "merge"
-      add_method = "concat"
-      collection = self_result
+  def owner_exists? user
+    if user.class <= Hash
+      username = user["username"]
     else
-    add_method = "push"
-      collection = [self_result]
+      username = user.username
     end
-    subgroup_array.each do |subgroup|
-      result = subgroup.send(key)
-      collection.send(add_method, result)
-    end
-    return collection
+    return self.owners.collect{|u| u.username}.include?(username)
   end
 
-  def members where = nil
-    memberships = self.get_subgroups("memberships")
-    if where
-      memberships = memberships.where(where)
-    end
-    fields = [:attendances, :student_observations, :submissions]
-    output = {}
-    memberships.each do |membership|
-      id = membership.user.id
-      fields.each do |field|
-        output[id] = {} if !output.has_key?(id)
-        output[id][:user] = membership.user if !output[id].has_key?(:user)
-        if !output[id].has_key?(field)
-          output[id][field] = membership.send(field)
-        else
-          output[id][field].concat(membership.send(field))
-        end
+  def bulk_create_memberships array, is_admin
+    array.each do |child|
+      user = User.find_by(username: child[0].downcase)
+      if !user
+        user = User.create!(username: child[0], name: child.join(" "), password: child[1].downcase)
       end
+      self.memberships.create(user_id: user.id, is_admin: is_admin)
     end
-    return output
   end
 
 end

app/models/membership.rb
@@ -61,7 +61,7 @@ class Membership < ActiveRecord::Base
       collection = self_result
       add_method = "concat"
     end
-    self.group.get_subgroups("memberships").each do |membership|
+    self.group.descendants_attr("memberships").each do |membership|
       next if membership.user.id != self.user.id
       collection.send(add_method, membership.send(key))
     end

app/models/tree.rb
@@ -1,0 +1,85 @@
+class Tree < ActiveRecord::Base
+  self.abstract_class = true
+  before_destroy :destroy_descendants
+
+  def ancestors(collection = nil)
+    collection = collection || [self]
+    if self.parent
+      collection.unshift(self.parent)
+      self.parent.ancestors(collection)
+    end
+    return collection
+  end
+
+  def descendants(collection = nil)
+    collection = collection || [self]
+    self.children.each do |child|
+      collection.push(child)
+      child.descendants(collection)
+    end
+    return collection
+  end
+
+  def descendant_tree
+    tree = self.as_json
+    children = []
+    self.children.each do |child|
+      children.push(child.descendant_tree)
+    end
+    tree[:children] = children
+    return tree
+  end
+
+  def ancestors_attr key
+    collection = []
+    collection.concat(self.send(key))
+    self.ancestors.each do |ancestor|
+      collection.concat(ancestor.send(key))
+    end
+    return collection.reverse
+  end
+
+  def descendants_attr key
+    collection = []
+    collection.concat(self.send(key))
+    self.descendants.each do |descendant|
+      collection.concat(descendant.send(key))
+    end
+    return collection
+  end
+
+  def is_descendant? ancestor
+    return true if self.id == ancestor.id
+    while self.parent
+      if self.parent.id == ancestor.id
+        return true
+      else
+        return false
+      end
+    end
+  end
+
+  # submissions group.submissions
+  def extract_descendants_from collection, belongs_to
+    output = []
+    descendants = self.descendants
+    collection.each do |record|
+      output.push(record) if descendants.include?(record.send(belongs_to))
+    end
+    return output
+  end
+
+  def create_descendants hash, name
+    hash.each do |key, subtree|
+      child = self.children.new
+      child.send("#{name}=", key)
+      child.save!
+      child.create_descendants(subtree, name)
+    end
+  end
+
+  def destroy_descendants
+    self.class.destroy_all(parent_id: self.id)
+  end
+
+end

app/views/groups/admin_dashboard.html.erb
@@ -23,12 +23,12 @@
     <th title="Total in which it was possible to get 2">Tot Poss</th>
     <% end %>
   </tr>
-<% @students.each do |id, student| %>
+<% @students.each do |student| %>
   <tr>
-    <td><%= avatar(student[:user]) %></td>
-    <th><%= link_to student[:user].username, group_student_path(@group.id, id) %></th>
+    <td><%= avatar(student.user) %></td>
+    <th><%= link_to student.user.username, group_student_path(@group.id, student.id) %></th>
     <% [:submissions, :attendances, :student_observations].each do |datum| %>
-      <%= render partial: "students/data", locals: {collection: student[datum]} %>
+      <%= render partial: "students/data", locals: {collection: student.send(datum)} %>
     <% end %>
   </tr>
 <% end %>

commit 0a8bda85e8ee58cc7d6a5bb5ea4b1bdd8fdc1228

hotfix for bug introduced in 9e23f738b50b3c009efbe5284919185ec0817916 when groups became trees

app/controllers/groups_controller.rb
@@ -10,7 +10,7 @@ class GroupsController < ApplicationController
     @admins = @group.admins
     @users = @group.nonadmins
     @is_admin = @admins.include?(current_user)
-    @observations = @group.descendants_attr("observations").uniq.sort{|a, b| a.created_at <=> b.created_at}
+    @observations = @group.descendants_attr("observations").sort{|a, b| a.created_at <=> b.created_at}
   end
 
   def create