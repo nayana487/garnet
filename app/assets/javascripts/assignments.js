083774b5f1c7d24ea526d4e32239f9de3ffd20b4
Jesse Shawl <jesse@jshawl.com>
2015-10-27 15:12:49 -0400

    wip moving assignments client-side

--- app/assets/javascripts/assignments.js
+++ app/assets/javascripts/assignments.js
@@ -1,27 +1,28 @@
 $(function(){
   var prsSubmitted = 0
-  $("[data-github-pr-submitted]").each(function(){
+  $("[data-github-pr-submitted]").each(isPrSubmitted)
+  function getIssues(url, issues, callback){
+    issues = issues || []
+    $.getJSON( url, function( res, textStatus, req ){
+      res.forEach( function( issue ){
+	issues.push( issue )
+      });
+      nextUrl = req.getAllResponseHeaders().match(/<(.*)>; rel="next"/);
+      if( nextUrl ){
+	getIssues(nextUrl[1], issues, callback);
+      } else {
+	callback(issues);
+      }
+    });
+  }
+  function isPrSubmitted(){
     var self = $(this)
+    console.log(self)
     var url = $(this).attr("data-github-pr-submitted")
     console.log(url)
-    $.getJSON(url, function(res){
-      console.log(res)
-      if(res.github_pr_submitted){
-	prsSubmitted++
-	var string = "<a href='"+res.github_pr_submitted.html_url+"'>View Pull Request.</a>"
-        if(res.github_pr_submitted.state == "open"){
-	  string += "<small> (open) </small>"
-	}
-      }else{
-        var string = "Missing." 
-      }
-      $("[data-percent-complete]").html(function(){
-	return ((prsSubmitted / $("[data-github-pr-submitted").length) * 100) + "%"
-      })
-      $("[data-percent-incomplete]").html(function(){
-	return 100 - ((prsSubmitted / $("[data-github-pr-submitted").length) * 100) + "%"
-      })
-      self.html(string)
+    getIssues(url, [], function(issues){
+      console.log("got all issues")
+      console.log(issues)
     })
-  })
+  }
 })
