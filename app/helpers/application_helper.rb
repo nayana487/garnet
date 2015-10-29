784306f1f9508639ffb78ab70136f1726644bea3
Andy <andrew.sunglae.kim@gmail.com>
2015-10-29 16:26:00 -0400

    adds markdown support: fixes #134

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -127,9 +127,27 @@ module ApplicationHelper
   end
 
   def markdown(text)
-    # markdown_to_html = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
-    # markdown_to_html.render(text).html_safe
-    return text
+    options = {
+      filter_html:     true,
+      hard_wrap:       true,
+      link_attributes: { rel: 'nofollow', target: "_blank" },
+      space_after_headers: true,
+      fenced_code_blocks: true
+    }
+
+    extensions = {
+      autolink:           true,
+      superscript:        true,
+      disable_indented_code_blocks: true,
+      fenced_code_blocks: true
+    }
+    if text.blank?
+      nil
+    else
+      renderer = Redcarpet::Render::HTML.new(options)
+      markdown = Redcarpet::Markdown.new(renderer, extensions)
+      markdown.render(text).html_safe
+    end
   end
 
   def is_admin_of_anything? user
