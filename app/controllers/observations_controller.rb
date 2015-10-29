67ed3c7c29a64678eeb2fd39159bfbdaf9c526cb
RobertAKARobin <robertgfthomas@gmail.com>
2015-10-29 18:52:33 -0400

    consolidated observations controlller

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -7,18 +7,22 @@ class ObservationsController < ApplicationController
   end
 
   def create
-    @group = Group.at_path(params[:group_path])
-    @user = User.named(params[:membership_user])
+    @user = User.named(params[:user_user])
     @observation = current_user.admin_observations.new(observation_params)
-    @observation.user_id = @user.id
-    @observation.group_id = @group.id
-    if @observation.save!
-      redirect_to group_membership_path(@group, @user)
-    end
+    @observation.user = @user
+    @observation.group = Group.find(observation_params["group_id"])
+    @observation.save!
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    @observation = Observation.find(params[:id])
+    @observation.destroy!
+    redirect_to user_path(@observation.user)
   end
 
   private
   def observation_params
-    params.require(:observation).permit(:body, :status)
+    params.require(:observation).permit(:group_id, :body, :status)
   end
 end
