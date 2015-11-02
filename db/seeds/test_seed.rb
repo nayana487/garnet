dceac34f5c1c897e39ed99e4e7c3b722ee43fe30
mattscilipoti <matt.scilipoti@ga.co>
2015-11-02 18:00:01 -0500

    group issues: add link to assignment issues for this group
    - seed: added testwdidcstudent
      - added assignment

--- db/seeds/test_seed.rb
+++ db/seeds/test_seed.rb
@@ -1,6 +1,7 @@
 load "#{Rails.root}/db/seeds.rb"
 # ActiveRecord::Base.logger = Logger.new(STDOUT)
 
+test_user = User.create(username: "TestWDIDCStudent", password: 'foo', github_id: 13137527)
 jane = User.create(username: "jane", password: "foo")
 john = User.create(username: "john", password: "foo")
 alice = User.create(username: "alice", password: "foo")
@@ -15,6 +16,10 @@ Group.at_path("ga_wdi_dc_7_squad-adam").add_admin(adam)
 
 Group.at_path("ga_wdi_dc_7_squad-adam").add_member(jane)
 Group.at_path("ga_wdi_dc_7_squad-adam").add_member(john)
+Group.at_path("ga_wdi_dc_7_squad-adam").add_member(test_user)
 Group.at_path("ga_wdi_dc_7").add_member(alice)
 Group.at_path("ga_wdi_dc_7").add_member(bob)
 Group.at_path("ga_wdi_dc_7").add_member(carol)
+
+squad_adam = Group.at_path("ga_wdi_dc_7_squad-adam")
+squad_adam.assignments.create!(title: "Test Assignment1", repo_url: "example.com/test_repo")
