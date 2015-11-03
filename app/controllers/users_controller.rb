5f2b31198d5651f88895ed6cb35ed7a28b9d50da
RobertAKARobin <robertgfthomas@gmail.com>
2015-11-03 13:59:05 -0500

    added back orphans route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
 
   skip_before_action :authenticate, except: [:show]
 
+  def orphans
+    @users = User.all.select{|u| u.groups.count < 1}
+    render :index
+  end
+
   def show
     if params[:user]
       if User.exists?(username: params[:user])
