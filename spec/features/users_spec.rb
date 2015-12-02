ac05b9f22c1d6ebb33bb39abd0bad8b1ef8781a9
Adam Bray <adam.bray@yale.edu>
2015-12-02 12:21:02 -0500

    remove incomplete and duplicate tests

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -2,19 +2,4 @@ require 'rails_helper'
 require_relative 'helpers/login_helper'
 
 RSpec.describe "user pages", :type => :feature do
-  before :each do
-    User.create(:username => 'testuser', :password => 'password')
-  end
-
-  scenario "sign in via username & password" do
-    sign_in_as('testuser', 'password')
-    expect(page).to have_content "You're signed in"
-  end
-
-  scenario "sign_out" do
-    sign_in_as('testuser', 'password')
-
-    click_on "sign out"
-    expect(page).to have_content "signed out"
-  end
 end
