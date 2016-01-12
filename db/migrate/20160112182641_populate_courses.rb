ac0a281d27758c6098ca7bb762d0a55b19959548
Adam Bray <adam.bray@yale.edu>
2016-01-12 14:28:13 -0500

    adds courses model with pre-populated data

---
+++ db/migrate/20160112182641_populate_courses.rb
@@ -0,0 +1,23 @@
+class PopulateCourses < ActiveRecord:                                               :Migration
+  def up
+    Course.create!([
+      { name: 'Web Development Immersive',        short_name: 'WDI',  format: 'Immersive' },
+      { name: 'User Experience Design Immersive', short_name: 'UXDI', format: 'Immersive' },
+      { name: 'Product Management Immersive',     short_name: 'PMI',  format: 'Immersive' },
+      { name: 'Android Development Immersive',    short_name: 'ADI',  format: 'Immersive' },
+      { name: 'User Experience Design',           short_name: 'UXD',  format: 'Course' },
+      { name: 'Digital Marketing',                short_name: 'DGM',  format: 'Course' },
+      { name: 'Data Science',                     short_name: 'DAT',  format: 'Course' },
+      { name: 'Data Analytics',                   short_name: 'ANA',  format: 'Course' },
+      { name: 'iOS Mobile Development',           short_name: 'MOB',  format: 'Course' },
+      { name: 'Front-End Web Development',        short_name: 'FEWD', format: 'Course' },
+      { name: 'Back-End Web Development',         short_name: 'BEWD', format: 'Course' },
+      { name: 'Product Management',               short_name: 'PDM',  format: 'Course' },
+      { name: 'Javascript',                       short_name: 'JS',   format: 'Course' }
+      ])
+    end
+
+    def down
+      Location.destroy_all
+    end
+  end
