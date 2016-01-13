2da35a8b9995ecfe428683147684330bd301fef1
Adam Bray <adam.bray@yale.edu>
2016-01-13 17:47:05 -0500

    remove tests related to groups and squads

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Event do
-  let(:group) { Group.last } #any group
+  let(:cohort) { Cohort.last } #any cohort
 
   context "when created" do
-    it "creates attendances for all members of the event's group"
+    it "creates attendances for all members of the event's cohort"
   end
 
-  describe "(avoiding duplicates, in the same group)" do
+  describe "(avoiding duplicates, in the same cohort)" do
     it "does NOT allow events within #{Event.duplicate_date_delta / 1.minute} minutes of each other" do
-      first_event = group.events.create!(date: (Event.duplicate_date_delta - 1).ago, title: "FIRST")
-      too_soon = group.events.build(date: Time.now, title: "TOO SOON")
+      first_event = cohort.events.create!(date: (Event.duplicate_date_delta - 1).ago, title: "FIRST")
+      too_soon = cohort.events.build(date: Time.now, title: "TOO SOON")
       expect(too_soon).to_not be_valid
       expect(too_soon.errors[:date]).to include(/occurs too close/)
     end
 
     it "does allow another event, after #{Event.duplicate_date_delta / 1.minute} minutes" do
-      first_event = group.events.create!(date: (Event.duplicate_date_delta + 1).ago, title: "FIRST")
-      enough_delay = group.events.build(date: Time.now, title: "ENOUGH DELAY")
+      first_event = cohort.events.create!(date: (Event.duplicate_date_delta + 1).ago, title: "FIRST")
+      enough_delay = cohort.events.build(date: Time.now, title: "ENOUGH DELAY")
       expect(enough_delay).to be_valid
     end
   end
