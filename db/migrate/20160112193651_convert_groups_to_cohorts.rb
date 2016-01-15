7b5a65010115cf268c8ac6f3c26bee9318b940c5
Adam Bray <adam.bray@yale.edu>
2016-01-15 09:39:16 -0500

    updates group conversion migration to only run if existing groups are present

--- db/migrate/20160112193651_convert_groups_to_cohorts.rb
+++ db/migrate/20160112193651_convert_groups_to_cohorts.rb
@@ -5,29 +5,33 @@ class ConvertGroupsToCohorts < ActiveRecord::Migration
     add_column :memberships, :cohort_id, :integer, references: "cohorts"
 
     wdi_group = Group.at_path("ga_wdi_dc_7")
-    wdi_cohort = Cohort.create!(name: "DC WDI7",
-                                start_date: Date.parse("2015-10-12"),
-                                end_date: Date.parse("2016-01-15"),
-                                course: Course.find_by(short_name: "WDI"),
-                                location: Location.find_by(short_name: "DC"))
+    if wdi_group
+      wdi_cohort = Cohort.create!(name: "DC WDI7",
+                                  start_date: Date.parse("2015-10-12"),
+                                  end_date: Date.parse("2016-01-15"),
+                                  course: Course.find_by(short_name: "WDI"),
+                                  location: Location.find_by(short_name: "DC"))
+
+      wdi_cohort.memberships = wdi_group.memberships
+      wdi_cohort.assignments = wdi_group.assignments
+      wdi_cohort.events = wdi_group.events
+      wdi_cohort.save!
+    end
 
     pmi_group = Group.at_path("ga_pmi_dc_1")
-    pmi_cohort = Cohort.create!(name: "DC PMI1",
-                                start_date: Date.parse("2015-11-09"),
-                                end_date: Date.parse("2016-01-29"),
-                                course: Course.find_by(short_name: "PMI"),
-                                location: Location.find_by(short_name: "DC"))
+    if pmi_group
+      pmi_cohort = Cohort.create!(name: "DC PMI1",
+                                  start_date: Date.parse("2015-11-09"),
+                                  end_date: Date.parse("2016-01-29"),
+                                  course: Course.find_by(short_name: "PMI"),
+                                  location: Location.find_by(short_name: "DC"))
 
 
-    wdi_cohort.memberships = wdi_group.memberships
-    wdi_cohort.assignments = wdi_group.assignments
-    wdi_cohort.events = wdi_group.events
-    wdi_cohort.save!
-
-    pmi_cohort.memberships = pmi_group.memberships
-    pmi_cohort.assignments = pmi_group.assignments
-    pmi_cohort.events = pmi_group.events
-    wdi_cohort.save!
+      pmi_cohort.memberships = pmi_group.memberships
+      pmi_cohort.assignments = pmi_group.assignments
+      pmi_cohort.events = pmi_group.events
+      wdi_cohort.save!
+    end
   end
 
   def down
