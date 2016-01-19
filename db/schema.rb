2efaaf5d709652810660792374b0bd699ec82b0d
Adam Bray <adam.bray@yale.edu>
2016-01-18 22:40:56 -0500

    fixes bug where memberships weren't transferred for PMI in the migrations. Fixes bug where memberships could have null for 'is_owner'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112212517) do
+ActiveRecord::Schema.define(version: 20160112212600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,7 +88,7 @@ ActiveRecord::Schema.define(version: 20160112212517) do
   create_table "memberships", force: :cascade do |t|
     t.integer  "group_id"
     t.integer  "user_id"
-    t.boolean  "is_owner"
+    t.boolean  "is_owner",                null: false
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.boolean  "is_priority"
