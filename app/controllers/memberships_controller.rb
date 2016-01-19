5a80b22d44e1d3e3bae2c0d6b295a11692175293
Adam Bray <adam.bray@yale.edu>
2016-01-18 21:01:05 -0500

    renames is_owner to is_admin and cleans up all uses of owner to admin

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -4,12 +4,12 @@ class MembershipsController < ApplicationController
 
   def create
     @cohort = Cohort.find(params[:cohort_id])
-    @is_owner = params[:is_owner]
+    @is_admin = params[:is_admin]
     @usernames = params[:usernames].downcase.split(/[ ,]+/)
     @usernames.each do |username|
       user = User.named(username)
       if !user then raise "I couldn't find a user named #{username}!" end
-      @membership = @cohort.memberships.create!(user: user, is_owner: @is_owner)
+      @membership = @cohort.memberships.create!(user: user, is_admin: @is_admin)
     end
     flash[:notice] = "Added #{@membership.user.username} to #{@cohort.name}!"
     redirect_to :back
@@ -37,6 +37,6 @@ class MembershipsController < ApplicationController
     end
 
     def membership_params
-      params.require(:membership).permit(:user_id, :is_owner)
+      params.require(:membership).permit(:user_id, :is_admin)
     end
 end
