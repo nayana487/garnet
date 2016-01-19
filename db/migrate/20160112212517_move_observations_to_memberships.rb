ebd5ced99627194b7c68281f577e022cc1c38f8d
Adam Bray <adam.bray@yale.edu>
2016-01-18 22:12:59 -0500

    fixes bug in migration script for migrating observations

--- db/migrate/20160112212517_move_observations_to_memberships.rb
+++ db/migrate/20160112212517_move_observations_to_memberships.rb
@@ -1,13 +1,26 @@
 class MoveObservationsToMemberships < ActiveRecord::Migration
   def up
     add_column :observations, :membership_id, :integer, references: "memberships"
-    
+
+    wdi = Group.at_path("ga_wdi_dc_7")
+    pmi = Group.at_path("ga_pmi_dc_1")
     Observation.all.each do |obs|
-      matching_membership = Membership.find_by(user: obs.user, group: obs.group)
-      if matching_membership
-        obs.membership = matching_membership
+      group = Group.find(obs.group_id)
+      if group == wdi || group.ancestors.include?(wdi)
+        new_group = wdi
+      elsif group == pmi || group.ancestors.include?(pmi)
+        new_group = pmi
+      elsif group == Group.at_path("ga")
+        new_group = wdi
       else
-        obs.membership = Membership.find_by(user: obs.user, group: Group.at_path("ga_wdi_dc_7"))
+        raise "Ambiguous Group Membership, #{obs.id}"
+      end
+      matching_membership = Membership.find_by(user_id: obs.user_id, group_id: new_group.id)
+      if matching_membership
+        obs.membership_id = matching_membership.id
+      else
+        # obs.membership_id = Membership.find_by(user_id: obs.user_id, group: Group.at_path("ga_wdi_dc_7")).id
+        raise "no membership found"
       end
       obs.save!
     end
