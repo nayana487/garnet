d7e75ede0be0e5ea1fa9ea1aa1587aa02014221b
Jesse Shawl <jesse@jshawl.com>
2016-01-25 10:56:12 -0500

    add tests for calculating attendance-status

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -7,12 +7,21 @@ RSpec.describe Attendance do
   describe "#calculate_status" do
     before(:each) do
       @att = Attendance.last
-      p @att
     end
     it "is present if before 9 am" do
       now = Time.parse("1969-07-20 08:59:59")
       allow(Time).to receive(:now) { now }
       expect(@att.calculate_status).to eq(2)
+    end
+    it "is tardy if before 12pm" do
+      now = Time.parse("1969-07-20 11:59:59")
+      allow(Time).to receive(:now) { now }
+      expect(@att.calculate_status).to eq(1)
+    end
+    it "is absent if after 1pm" do
+      now = Time.parse("1969-07-20 13:00:00")
+      allow(Time).to receive(:now) { now }
+      expect(@att.calculate_status).to eq(0)
     end
   end
 end
