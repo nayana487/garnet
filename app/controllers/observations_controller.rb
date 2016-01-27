e3137114bfa5971a25ac51ed36128d31a9d0fcf8
Adam Bray <adam.bray@yale.edu>
2016-01-27 10:51:33 -0500

    fixes bug in creating observations

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -2,8 +2,11 @@ class ObservationsController < ApplicationController
 
   def create
     observation = current_user.admin_observations.new(observation_params)
-    observation.save!
-    redirect_to observation.user
+    if observation.save
+      redirect_to observation.membership
+    else
+      redirect_to :back, notice: "Error saving observation"
+    end
   end
 
   def destroy
