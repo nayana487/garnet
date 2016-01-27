fe851a31a687e2e10b953e17afdaab0b87f7d8ef
Becky Beauchamp <beckybeauchamp1@gmail.com>
2016-01-27 16:15:26 -0500

    Refactors Scores To Submit Based on Comments

--- app/assets/javascripts/submission.js
+++ app/assets/javascripts/submission.js
@@ -1,20 +1,18 @@
-$(".submission-score").on("keydown", function(evt){
-  if(evt.keyCode == 13){
-    var self = $(this);
-    var submissionId = $(this).attr("submission-id");
-    var url = "../submissions/" + submissionId;
-    $.ajax({
-      url: url,
-      dataType: 'json',
-      type: "put",
-      data: {
-          score: self.val()
-      }
-    }).done(function(res){
-      console.log(res)
-      self.parent().parent().append($("<td>Score Update</td>"))
-    }).fail(function(res){
-      console.log("this failed");
-    });
-  }
+$(".score-update-form").on("submit", function(evt){
+  evt.preventDefault();
+  var inputEl = $(this).children(".submission-score").eq(0);
+  var submissionId = inputEl.attr("submission-id");
+  var url = "../submissions/" + submissionId;
+  $.ajax({
+    url: url,
+    dataType: 'json',
+    type: "put",
+    data: {
+      score: inputEl.val()
+    }
+  }).done(function(res){
+    inputEl.parents("tr").append($("<td>Score Update</td>"))
+  }).fail(function(res){
+    console.log("this failed");
+  });
 });
