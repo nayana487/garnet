2c66af8ea78a94430065290b3e55cf81b8f63ed7
Nick Olds <nolds9@yahoo.com>
2016-02-02 10:02:44 -0500

    adds view + specs for member's average observations

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe Membership do
   before(:all) do
     load "#{Rails.root}/db/seeds/test_seed.rb"
+    cohort = Cohort.last
+    @m = cohort.memberships.create()
+    @m.observations.create(status: 0)
+    @m.observations.create(status: 0)
+    @m.observations.create(status: 1)
   end
 
   let(:ga_root_group) { Group.at_path("ga") }
@@ -13,6 +18,12 @@ RSpec.describe Membership do
     end
     it "has many tags through taggings" do
       should have_many(:tags)
+    end
+  end
+
+  describe "average observations" do
+    it "has an average observations method" do
+      expect(@m.average_observations).to eq(0.33)
     end
   end
 
