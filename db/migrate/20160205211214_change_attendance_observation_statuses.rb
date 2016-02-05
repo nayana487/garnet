686c691ae6b3d277884472e8dbcf543db5c9f913
Adrian Maseda <amaseda@gmail.com>
2016-02-05 16:46:57 -0500

    add down method to status_update migration

--- db/migrate/20160205211214_change_attendance_observation_statuses.rb
+++ db/migrate/20160205211214_change_attendance_observation_statuses.rb
@@ -1,33 +1,35 @@
 class ChangeAttendanceObservationStatuses < ActiveRecord::Migration
-  def up
 
-    class MigrationAttendance < ActiveRecord::Base
-      self.table_name = :attendances
-    end
-
-    class MigrationSubmission < ActiveRecord::Base
-      self.table_name = :submissions
-    end
-
-    def convert_status class_to_be_changed
-      nil_attendances = class_to_be_changed.where(status:nil)
-      nil_attendances.update_all(status:0)
-      non_nil_attendances = class_to_be_changed.all - nil_attendances
-      non_nil_attendances.update_all("set status = status + 1")
-    end
-
-    convert_status MigrationAttendance
-    convert_status MigrationSubmission
-
-    # nil_attendances = MigrationAttendance.where(status:nil)
-    # nil_attendances.update_all(status:0)
-    # non_nil_attendances = MigrationAttendance.all - nil_attendances
-    # non_nil_attendances.update_all("set status = status + 1")
-    #
-    # nil_submissions = MigrationSubmission.where(status:nil)
-    # nil_submissions.update_all(status:0)
-    # non_nil_submissions = MigrationSubmission.all - nil_submissions
-    # non_nil_submissions.update_all("set status = status + 1")
-
+  class MigrationAttendance < ActiveRecord::Base
+    self.table_name = :attendances
   end
+
+  class MigrationSubmission < ActiveRecord::Base
+    self.table_name = :submissions
+  end
+
+  def convert_status_to_ar_enums class_to_be_changed
+    nil_status_instances = class_to_be_changed.where(status:nil)
+    nil_status_instances.update_all(status:0)
+    non_nil_status_instances = class_to_be_changed.where.not(status:nil)
+    non_nil_status_instances.update_all("status = status + 1")
+  end
+
+  def convert_status_to_class_methods class_to_be_changed
+    zero_status_instances = class_to_be_changed.where(status:0)
+    zero_status_instances.update_all(status:nil)
+    non_zero_status_instances = class_to_be_changed.where.not(status:0)
+    non_zero_status_instances.update_all("status = status - 1")
+  end
+
+  def up
+    convert_status_to_ar_enums MigrationAttendance
+    convert_status_to_ar_enums MigrationSubmission
+  end
+
+  def down
+    convert_status_to_class_methods MigrationAttendance
+    convert_status_to_class_methods MigrationSubmission
+  end
+
 end
