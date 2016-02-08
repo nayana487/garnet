4f256885b7706853700eb7f6358b897b93fff964
Adam Bray <adam.bray@yale.edu>
2016-02-08 16:49:21 -0500

    fixes observations so they only show on the correct page

--- app/models/user.rb
+++ app/models/user.rb
@@ -84,13 +84,6 @@ class User < ActiveRecord::Base
     self.cohorts & user.adminned_cohorts
   end
 
-  def records_accessible_by user, attribute_name
-    records = self.send(attribute_name)
-    my_cohorts = records.collect(&:cohort).uniq
-    common_cohorts = my_cohorts & user.adminned_cohorts
-    return records.select{|r| common_cohorts.include?(r.cohort)}
-  end
-
   def is_admin_of_anything?
     self.memberships.exists?(is_admin: true)
   end
