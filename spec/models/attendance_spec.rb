aa29c5b291aecded2ac2aad5b346bb3b1de55dd8
Andy <andrew.sunglae.kim@gmail.com>
2016-02-10 12:38:21 -0500

    update attendance spec to use enum statuses instead of integers

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -13,17 +13,17 @@ RSpec.describe Attendance do
     it "is present if before 9 am" do
       now = Time.parse("1969-07-20 08:59:59")
       allow(Time).to receive(:now) { now }
-      expect(@att.calculate_status).to eq(3)
+      expect(@att.calculate_status).to eq(Attendance.statuses[:present])
     end
     it "is tardy if before 12pm" do
       now = Time.parse("1969-07-20 11:59:59")
       allow(Time).to receive(:now) { now }
-      expect(@att.calculate_status).to eq(2)
+      expect(@att.calculate_status).to eq(Attendance.statuses[:tardy])
     end
     it "is absent if after 1pm" do
       now = Time.parse("1969-07-20 13:00:00")
       allow(Time).to receive(:now) { now }
-      expect(@att.calculate_status).to eq(1)
+      expect(@att.calculate_status).to eq(Attendance.statuses[:absent])
     end
   end
 end
