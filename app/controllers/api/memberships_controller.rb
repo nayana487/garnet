66c644c4cfe71e0f2e4e70c8ca0543a02bc102fd
Andy <andrew.sunglae.kim@gmail.com>
2016-02-12 10:27:59 -0500

    test open

--- app/controllers/api/memberships_controller.rb
+++ app/controllers/api/memberships_controller.rb
@@ -5,12 +5,13 @@ module Api
       @cohort = Cohort.find(params[:cohort_id])
       if can? :list_cohort_members, @cohort
         @names = @cohort.memberships.map do |member|
-          member.user.name
+          member.user.name if member.tags.includes?(params[:tag])
         end
         render json: @names, callback: params[:callback]
       else
         return render json: {error: "Not Authorized"}, callback: params[:callback]
       end
     end
+
   end
 end
--- spec/features/api_spec.rb
+++ spec/features/api_spec.rb
@@ -3,9 +3,16 @@ require 'rails_helper'
 RSpec.feature "api spec", :type => :feature do
   before(:all) do
     load "#{Rails.root}/db/seeds/test_seed.rb"
+    @user = User.last
+    @user_membership = @user.memberships.first
+    @cohort = @user_membership.cohort
+    @other_user_membership = @cohort.memberships.where.not(user_id: @user.id).first
+    @user_tag = @user_membership.tags.create(name: "hotdog")
+    @other_user_tag = @other_user_membership.tags.create(name: "hamburger")
   end
   let(:test_instructor) { User.create!(:username => 'test_instructor', :password => 'password') }
   let(:test_instructor2) { User.create!(:username => 'test_instructor2', :password => 'password') }
+
 
   scenario "get information about a user" do
     login_user(test_instructor)
@@ -14,4 +21,13 @@ RSpec.feature "api spec", :type => :feature do
     visit api_user_path + "?api_token=" + test_instructor2.api_token
     expect(page).to have_content("test_instructor2")
   end
+
+  scenario "filter memberships by tag" do
+    @user.generate_api_token
+    visit api_user_path + "?api_token=" + @user.api_token + "&tag=#{@other_user_tag.name}"
+    expect(page).not_to have_content(@user.name)
+  end
+
+
+
 end
