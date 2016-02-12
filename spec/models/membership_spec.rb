d53be6e83f3f4d4c6fca1627287a7ceef556cdf5
Jesse Shawl <jesse@jshawl.com>
2016-02-12 12:14:32 -0500

    convert stati to enums

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -9,12 +9,11 @@ RSpec.describe Membership do
     @m.observations.create(status: 0)
     @m.observations.create(status: 1)
     cohort.events.create!(title:"Some test event2", occurs_at: Time.now)
-    @m.attendances.first.update(status:2)
+    @m.attendances.first.present!
     cohort.events.create!(title:"Some test event", occurs_at: Time.now + 3.hours)
     cohort.assignments.create!(title: "Some test assignment")
     cohort.assignments.create!(title: "Some test assignment #2")
-    @m.submissions.first.update(status: 2)
-
+    @m.submissions.first.complete!
   end
 
   let(:ga_root_group) { Group.at_path("ga") }
@@ -50,10 +49,10 @@ RSpec.describe Membership do
 
   describe "percent from status" do
     it "should not include future attendances" do
-      expect(@m.percent_from_status(:attendances, 2)).to eq(100)
+      expect(@m.percent_from_status(:attendances, Attendance.statuses[:present])).to eq(100)
     end
     it "should exclude n/a assignments" do
-      expect(@m.percent_from_status(:submissions, 2)).to eq(100)
+      expect(@m.percent_from_status(:submissions, Submission.statuses[:complete])).to eq(100)
     end
   end
 end
