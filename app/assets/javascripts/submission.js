23c18c1f494608942c904486abd2fe735db7b383
Adam Bray <adam.bray@yale.edu>
2016-02-22 13:48:37 -0500

    submits HW score on focusout

--- app/assets/javascripts/submission.js
+++ app/assets/javascripts/submission.js
@@ -1,6 +1,11 @@
-$(".js-score-update-form").on("submit", function(evt){
+$(".js-score-update-form").on("submit", submitScore);
+$(".js-score-update-form input").on("focusout", submitScore);
+
+function submitScore(evt){
   evt.preventDefault();
-  var inputEl = $(this).children(".submission-score").eq(0);
+
+  var form = $(this).closest("form");
+  var inputEl = form.find(".submission-score").eq(0);
   var submissionId = inputEl.attr("submission-id");
   var url = "../submissions/" + submissionId;
   $(inputEl).addClass("waiting");
@@ -16,4 +21,4 @@ $(".js-score-update-form").on("submit", function(evt){
   }).always(function(){
     $(inputEl).removeClass("waiting");
   });
-});
+}
