45b785342e6f5c228d35a877418359992c0fd8b8
RobertAKARobin <robertgfthomas@gmail.com>
2016-02-25 14:28:36 -0500

    Sweet button animations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -88,15 +88,19 @@ function autoSave(evt){
   var form    = trigger.closest("form");
   var data    = form.serializeArray();
   var url     = form.attr("action");
+  trigger.removeClass("dirty");
   trigger.addClass("waiting");
   $.ajax({
     url:      url,
     type:     "post",
     dataType: "json",
     data:     data
-  }).fail(function(response){
+  }).fail(function(){
     console.log("That didn't work.");
   }).always(function(response){
+    console.log(response);
+  }).success(function(){
     trigger.removeClass("waiting");
+    trigger.addClass("dirty");
   });
 }
