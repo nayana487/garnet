f1ab6fd6b7f6c165535f78ab9081e27dd3a7a566
RobertAKARobin <robertgfthomas@gmail.com>
2016-03-09 15:10:22 -0500

    Refactored coloring of statuses and percents
    - A single "#color_of" helper method that takes a variety of inputs
    - Colors are rendered with `style` tags on HSL scale instead of with CSS classes
    - Specs included
    - We won't talk about how much math this took the first time around

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,37 +17,38 @@ module ApplicationHelper
     end
   end
 
-  def color_of_percent input, type = :good
-    # can't do calculations on nil values
-    return "" if input.nil?
-
-    # if the input is a 'bad' value, flip and scale the value accordingly.
-    # e.g. a good value would be %HW complete, and 75% would be green
-    #
-    # but a bad value might be "% absent", and 10% would be yellow
-    input = (100 - (input* 3)) if type == :bad
-
-    case input
-    when -1000...25
-      return "status_very_bad"
-    when 25...50
-      return "status_bad"
-    when 50...75
-      return "status_mediocre"
-    when 75..1000
-      return "status_good"
+  def color_of input, options = {}
+    # These are options that can optionally be passed in when the method is called
+    greenest_possible_input = options[:green]
+    reddest_possible_input  = options[:red]
+    type_of_output          = options[:type]
+    # HSL is a color method like RGB and CMYK. It stands for "Hue, Saturation, Lightness"
+    # If h is 120 it's green; 240 is blue; 360 and 0 are red
+    hsl_green_value = 120
+    hsl_red_value   = 0
+    if input.is_a? Numeric
+      greenest_possible_input  ||= 100
+      reddest_possible_input   ||= 0
+    else # elsif input is a record, like submission, attendance, observation, etc
+      greenest_possible_input  ||= (input.class.statuses.count - 1)
+      reddest_possible_input   ||= 1
+      input = input.read_attribute("status")
+      input = nil if (input == 0)
     end
-  end
-
-  def color_of_status input
-    case input
-    when "red"
-      return "status_very_bad"
-    when "yellow"
-      return "status_mediocre"
-    when "green"
-      return "status_good"
+    return "" if (input.nil?)
+    input_range = (greenest_possible_input - reddest_possible_input)
+    input_as_percentage = (input - reddest_possible_input).to_f / (input_range)
+    # How "green" is the input?
+    hsl_color_value = hsl_green_value * input_as_percentage
+    if hsl_color_value > hsl_green_value
+      hsl_color_value = [hsl_color_value, hsl_green_value].min
+    else # Makes sure the color doesn't go past green or red
+      hsl_color_value = [hsl_color_value, hsl_red_value].max
     end
+    hsl_string = "hsl(#{hsl_color_value},100%,90%)"
+    return hsl_color_value if (type_of_output === "int")
+    return hsl_string if (type_of_output === "hsl_string")
+    return "style='background-color:#{hsl_string};'".html_safe
   end
 
   def percent_of collection, value
