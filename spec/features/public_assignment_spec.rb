2e764cee78f6241591135a52a2aee95f6c675284
Tyler Crosse <tylerscottcrosse@gmail.com>
2016-03-11 16:38:18 -0500

    Finishes tests for public-private submission viewing feature

--- spec/features/public_assignment_spec.rb
+++ spec/features/public_assignment_spec.rb
@@ -9,58 +9,54 @@ RSpec.feature 'Submission Visibility' do
 
   let(:test_instructor)  { User.create!(:username => 'TEST_INSTRUCTOR',  :password => 'password') }
 
-  let(:john) { User.find_by(username: 'john') }
+  let(:sammy) { User.create!(username: 'sammy',  :password => 'password') }
 
   let(:test_instructor_membership) { Membership.find_by(user: test_instructor, cohort: cohort1) }
-  let(:john_membership) { Membership.find_by(user: john, cohort: cohort1) }
+  let(:sammy_membership) { Membership.find_by(user: sammy, cohort: cohort1) }
 
   let(:public_assignment) { cohort1.assignments.create(title: "Test Assignment", public: true, base_score: 10) }
   let(:private_assignment) { cohort1.assignments.create(title: "Test Assignment", public: false, base_score: 10) }
 
-  let(:public_submission) { public_assignment.submissions.find_by(membership: john_membership) }
-  let(:private_submission) { private_assignment.submissions.find_by(membership: john_membership) }
+  let(:public_submission) { public_assignment.submissions.find_by(membership: sammy_membership) }
+  let(:private_submission) { private_assignment.submissions.find_by(membership: sammy_membership) }
 
   before(:each) {
     cohort1.add_admin(test_instructor)
-    cohort1.add_member(john)
+    cohort1.add_member(sammy)
 
     public_submission.score = 7
     private_submission.score = 8
+
+    public_submission.save
+    private_submission.save
   }
 
-  scenario 'instructor can see scores for both public and private submissions' do
+  scenario 'instructor can see both public and private assignment submission scores' do
     login_user(test_instructor)
-    visit(membership_path(john_membership))
-    # expect(page).to have_content "Homework Submissions"
+    visit(membership_path(sammy_membership))
 
-    puts '8' + '=' * 50 + 'D'
-    # puts find_field("form[action='/submissions/#{public_submission.id}'] input.sm").value
-    puts page.find_field("form[action='/submissions/#{public_submission.id}'] input.sm").value
-    puts '8' + '=' * 50 + 'D'
+    expect(page).to have_selector("form[action='/submissions/#{public_submission.id}'] input[value='7']")
+    expect(page).to have_selector("form[action='/submissions/#{private_submission.id}'] input[value='8']")
   end
 
-  # scenario 'when signed in' do
-  #   login_user(test_instructor)
-  #   expect(page).to have_content "You're signed in, #{test_instructor.username}"
-  # end
-  #
-  # scenario 'viewing todos for events' do
-  #   login_user(test_instructor)
-  #   visit(root_path)
-  #   expect(page).to have_content "Jane"
-  #   expect(page).to have_content "Test Event"
-  # end
-  #
-  # scenario 'viewing todos for assignments' do
-  #   login_user(test_instructor)
-  #   visit(root_path)
-  #   expect(page).to have_content "Jane"
-  #   expect(page).to have_content "Test Assignment"
-  # end
-  #
-  # scenario 'inactive students are not shown on todos' do
-  #   login_user(test_instructor)
-  #   visit(root_path)
-  #   expect(page).to_not have_content "John"
-  # end
+  scenario 'student can not see private assignment submission scores' do
+    login_user(sammy)
+    visit(membership_path(sammy_membership))
+
+    within("tr[data-submission-id='#{private_submission.id}'] td.score") do
+      expect(page).to_not have_content("8")
+      expect(page).to have_content("N/A")
+    end
+  end
+
+  scenario 'student can see public assignment submission scores' do
+    login_user(sammy)
+    visit(membership_path(sammy_membership))
+
+    within("tr[data-submission-id='#{public_submission.id}'] td.score") do
+      expect(page).to_not have_content("N/A")
+      expect(page).to have_content("7")
+    end
+  end
+
 end
