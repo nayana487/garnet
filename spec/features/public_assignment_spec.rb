1ec8e73de2dedfbc43c27a47d90a6a9c7a73e4ac
Tyler Crosse <tylerscottcrosse@gmail.com>
2016-03-11 12:30:55 -0500

    Progress on tests

--- spec/features/public_assignment_spec.rb
+++ spec/features/public_assignment_spec.rb
@@ -11,19 +11,18 @@ RSpec.feature 'Submission Visibility' do
 
   let(:john) { User.find_by(username: 'john') }
 
+  let(:test_instructor_membership) { Membership.find_by(user: test_instructor, cohort: cohort1) }
   let(:john_membership) { Membership.find_by(user: john, cohort: cohort1) }
+
+  let(:public_assignment) { cohort1.assignments.create(title: "Test Assignment", public: true, base_score: 10) }
+  let(:private_assignment) { cohort1.assignments.create(title: "Test Assignment", public: false, base_score: 10) }
+
+  let(:public_submission) { public_assignment.submissions.find_by(membership: john_membership) }
+  let(:private_submission) { private_assignment.submissions.find_by(membership: john_membership) }
 
   before(:each) {
     cohort1.add_admin(test_instructor)
     cohort1.add_member(john)
-
-    test_instructor_membership = Membership.find_by(user: test_instructor, cohort: cohort1)
-
-    public_assignment = cohort1.assignments.create(title: "Test Assignment", public: true, base_score: 10)
-    private_assignment = cohort1.assignments.create(title: "Test Assignment", public: false, base_score: 10)
-
-    public_submission = public_assignment.submissions.find_by(membership: john_membership)
-    private_submission = private_assignment.submissions.find_by(membership: john_membership)
 
     public_submission.score = 7
     private_submission.score = 8
@@ -32,7 +31,12 @@ RSpec.feature 'Submission Visibility' do
   scenario 'instructor can see scores for both public and private submissions' do
     login_user(test_instructor)
     visit(membership_path(john_membership))
-    expect(page).to have_content "Homework Submissions"
+    # expect(page).to have_content "Homework Submissions"
+
+    puts '8' + '=' * 50 + 'D'
+    # puts find_field("form[action='/submissions/#{public_submission.id}'] input.sm").value
+    puts page.find_field("form[action='/submissions/#{public_submission.id}'] input.sm").value
+    puts '8' + '=' * 50 + 'D'
   end
 
   # scenario 'when signed in' do
