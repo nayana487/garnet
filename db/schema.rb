b32078bb9d928759098248f3ab974b5e6d1be85a
Becky Beauchamp <beckybeauchamp1@gmail.com>
2016-03-15 13:16:15 -0400

    Adds spec for user features

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20160306220325) do
   end
 
   create_table "attendances", force: :cascade do |t|
-    t.integer  "status"
+    t.integer  "status",        default: 0
     t.integer  "event_id"
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
@@ -45,8 +45,8 @@ ActiveRecord::Schema.define(version: 20160306220325) do
     t.date     "end_date"
     t.integer  "course_id"
     t.integer  "location_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "invite_code"
     t.integer  "number_at_location"
   end
@@ -73,6 +73,14 @@ ActiveRecord::Schema.define(version: 20160306220325) do
     t.integer  "cohort_id"
   end
 
+  create_table "groups", force: :cascade do |t|
+    t.string   "title"
+    t.string   "category"
+    t.integer  "parent_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "locations", force: :cascade do |t|
     t.string   "name"
     t.string   "short_name"
@@ -87,10 +95,10 @@ ActiveRecord::Schema.define(version: 20160306220325) do
 
   create_table "memberships", force: :cascade do |t|
     t.integer  "user_id"
-    t.boolean  "is_admin",               null: false
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.integer  "status",     default: 0
+    t.boolean  "is_admin",                         null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.integer  "status",               default: 0
     t.integer  "cohort_id"
     t.string   "percent_submissions"
     t.string   "percent_attendances"
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,4 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe "user pages", :type => :feature do
+RSpec.describe "user pages" do
+  let(:test_user) do
+    FactoryGirl.create(:user)
+  end
+  let(:cohort1) { FactoryGirl.create(:cohort) }
+  let(:test_instructor)  { User.create!(:username => 'TEST_INSTRUCTOR',  :password => 'password') }
+
+  let(:test_user_2) do
+    FactoryGirl.create(:user)
+  end
+  let(:test_user_2)  { User.create!(:username => 'TEST_USER_2',  :password => 'password') }
+
+
+
+  scenario "looking at user you admin" do
+    cohort1.add_admin(test_instructor)
+    cohort1.add_member(test_user)
+
+    login_user(test_instructor)
+    visit(user_path test_user)
+    expect(page).to have_css("[data-is-editable='true']")
+  end
+
+  scenario "looking at user you don't admin" do
+    cohort1.add_member(test_user)
+    cohort1.add_member(test_user_2)
+    login_user(test_user)
+    visit(user_path test_user_2)
+    save_and_open_page
+    expect(page).to have_css("[data-is-editable='false']")
+  end
+
 end
