e61620714817d3f5023499d951ed24bbe5c7bea8
Andy <andrew.sunglae.kim@gmail.com>
2016-03-15 16:56:32 -0400

    adds test for event generation for cohorts

--- spec/models/cohort_spec.rb
+++ spec/models/cohort_spec.rb
@@ -1,30 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Cohort do
-  before(:all) do
-    let(:test_cohort) { Cohort.create!(name: "Test Cohort" start_date: DateTime.parse("1969-07-20 08:34:59"), end_date: DateTime.parse("1969-07-27 08:59:59"))}
-    let(:test_student)  { User.create!(:username => 'test student',  :password => 'password') }
-  end
+  let(:test_cohort) { Cohort.create!(name: "Test Cohort", start_date: DateTime.parse("1969-07-20 08:34:59"), end_date: DateTime.parse("1969-07-27 08:59:59"))}
+  let(:start_time) { 9 }
   describe "#generate_events" do
 
     before(:each) do
-      test_cohort.add_member(test_student)
-      # @evt = Event.create(title: "Test Event", occurs_at: Time.parse("1969-07-20 09:00:00"), cohort: Cohort.last)
-      # @att = @evt.attendances.last
+      test_cohort.events.destroy_all
     end
     it "creates events with a specified start time" do
-
+      test_cohort.generate_events(start_time)
+      expect(test_cohort.events.sample.occurs_at.hour).to eq(start_time)
     end
 
     it "creates events for weekdays" do
-      now = Time.parse("1969-07-20 08:59:59")
-      allow(Time).to receive(:now) { now }
-      expect(@att.calculate_status).to eq(Attendance.statuses[:present])
+      test_cohort.generate_events(start_time)
+      num_weekdays = 5
+      num_events = test_cohort.events.length
+      expect(num_events).to eq(num_weekdays)
     end
+
     it "does not create events for weekends" do
-      now = Time.parse("1969-07-20 11:59:59")
-      allow(Time).to receive(:now) { now }
-      expect(@att.calculate_status).to eq(Attendance.statuses[:tardy])
+      test_cohort.generate_events(start_time)
+      # wday of a date time will return 0 for sunday and 6 for saturday, weekdays are 1 - 5
+      weekend_events = test_cohort.events.any?{|event| event.occurs_at.wday == 0 || event.occurs_at.wday == 6}
+      expect(weekend_events).to eq(false)
+    end
+
+    it "does not create duplicate events" do
+      test_cohort.generate_events(start_time)
+      num_events_before_running_method_again = test_cohort.events.length
+      test_cohort.generate_events(start_time)
+      num_events_after_running_method_again = test_cohort.events.length
+      expect(num_events_before_running_method_again).to eq(num_events_after_running_method_again)
     end
   end
 end
