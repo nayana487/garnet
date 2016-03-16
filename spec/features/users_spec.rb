2d9ef33ea8a07ae3b2ac967e7d676eaa45f4f28a
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2016-03-16 10:07:50 -0400

    completes testing for user edits

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -11,22 +11,21 @@ RSpec.describe "user pages" do
 
 
 
-  # scenario "looking at user you admin" do
-  #   cohort1.add_admin(test_instructor)
-  #   cohort1.add_member(test_user)
-  #
-  #   login_user(test_instructor)
-  #   visit(user_path test_user)
-  #   expect(page).to have_css("[data-is-editable='true']")
-  # end
+  scenario "looking at user you admin" do
+    cohort1.add_admin(test_instructor)
+    cohort1.add_member(test_user)
+
+    login_user(test_instructor)
+    visit(user_path test_user)
+    expect(page).to have_css("[data-is-editable='true']")
+  end
 
   scenario "looking at user you don't admin" do
     cohort1.add_member(test_user)
     cohort1.add_member(test_user_2)
     login_user(test_user)
     visit(user_path test_user_2)
-    save_and_open_page
-    expect(page).to have_css("[data-is-editable='false']")
+    expect(current_path).to_not eq(user_path(test_user_2))
   end
 
 end
