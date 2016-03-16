6aea9c448251508cce717b5c711b114431786d0c
Jesse Shawl <jesse@jshawl.com>
2016-03-16 13:51:32 -0400

    :fire: hotfix update origin for chrome extension

--- app/controllers/api/observations_controller.rb
+++ app/controllers/api/observations_controller.rb
@@ -11,7 +11,7 @@ module Api
 
     private
     def allow_cors
-      headers['Access-Control-Allow-Origin'] = 'https://outcomes.generalassemb.ly'
+      headers['Access-Control-Allow-Origin'] = 'chrome-extension://jnocagkfhahhecljommidimlhkjcginp'
       headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
       headers['Access-Control-Request-Method'] = '*'
       headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
