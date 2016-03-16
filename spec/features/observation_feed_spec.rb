e638708271faba8f9a6a6c696bed468cc4b01ba0
Adam Bray <adam.bray@yale.edu>
2016-03-16 12:34:38 -0400

    updates test to fail (and be more readable)

--- spec/features/observation_feed_spec.rb
+++ spec/features/observation_feed_spec.rb
@@ -12,6 +12,7 @@ RSpec.feature 'Observation Feed' do
 
   let(:john) { User.find_by(username: 'john') }
   let(:jane) { User.find_by(username: 'jane') }
+  let(:dave) { User.find_by(username: 'jane') }
 
   before(:each) {
     cohort1.add_admin(test_instructor)
@@ -20,36 +21,33 @@ RSpec.feature 'Observation Feed' do
     cohort1.add_member(jane)
 
     cohort2.add_admin(other_instructor)
-    cohort2.add_member(john)
+    cohort2.add_member(dave)
 
-    tag = Tag.create(name: "test")
+    john_cohort1 = john.memberships.find_by(cohort: cohort1)
+    jane_cohort1 = jane.memberships.find_by(cohort: cohort1)
+    dave_cohort2 = dave.memberships.find_by(cohort: cohort2)
 
-    john_membership = john.memberships.find_by(cohort: cohort1)
-    john_other_membership = john.memberships.find_by(cohort: cohort2)
-    jane_membership = jane.memberships.find_by(cohort: cohort1)
-
-    john_membership.observations.create(status: 1, body: "johns obs from test instructor", admin_id: test_instructor.id)
-    jane_membership.observations.create(status: 1, body: "janes obs from other test instructor", admin_id: other_instructor.id)
-    john_other_membership.observations.create(status: 1, body: "johns obs from other test instructor", admin_id: other_instructor.id)
+    @allowed_obs1 = john_cohort1.observations.create(status: 1, body: "john in cohort1", admin_id: test_instructor.id)
+    @allowed_obs2 = jane_cohort1.observations.create(status: 1, body: "jane in cohort1", admin_id: other_instructor.id)
+    @denied_obs1  = dave_cohort2.observations.create(status: 1, body: "dave in cohort2", admin_id: other_instructor.id)
   }
 
   scenario 'should see feed header' do
     login_user(test_instructor)
-    visit "/cohorts/#{cohort1.id}/observations"
+    visit observations_cohort_path(cohort1)
     expect(page).to have_content "Last 10 Observations"
   end
 
   scenario 'should see recent observations from all admins in cohort' do
     login_user(test_instructor)
-    visit "/cohorts/#{cohort1.id}/observations"
-    expect(page).to have_content "johns obs from test instructor"
-    expect(page).to have_content "janes obs from other test instructor"
+    visit observations_cohort_path(cohort1)
+    expect(page).to have_content @allowed_obs1.body
+    expect(page).to have_content @allowed_obs2.body
   end
 
   scenario 'should not see observations from other cohorts' do
     login_user(test_instructor)
-    visit "/cohorts/#{cohort1.id}/observations"
-    expect(page).to_not have_content "johns obs from other test instructor"
-
+    visit observations_cohort_path(cohort2)
+    expect(page).to_not have_content @denied_obs1.body
   end
 end
