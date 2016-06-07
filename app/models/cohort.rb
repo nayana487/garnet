49f2dc8bfc9ec14ba88e66b19f49429c62603228
Tyler Crosse <tylerscottcrosse@gmail.com>
2016-06-07 17:02:32 -0400

    Cohort: convert methods to associations

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,13 +1,22 @@
 class Cohort < ActiveRecord::Base
   has_many :events
-  has_many :attendances,  through: :events
+  has_many :attendances, through: :events
 
   has_many :assignments
-  has_many :submissions,  through: :assignments
+  has_many :submissions, through: :assignments
 
-  has_many :memberships,    dependent:  :destroy
-  has_many :users,          through:    :memberships
-  has_many :observations,   through:    :memberships
+  has_many :memberships, dependent:  :destroy
+  has_many :admin_memberships, -> { where(is_admin: true) }, class_name: 'Membership'
+  has_many :nonadmin_memberships, -> { where(is_admin: false) }, class_name: 'Membership'
+  alias_method :student_memberships, :nonadmin_memberships
+
+  # through memberships
+  has_many :admins, through: :admin_memberships, :source => :user
+  alias_method :instructors, :admins
+  has_many :nonadmins, through: :nonadmin_memberships, :source => :user
+  alias_method :students, :nonadmins
+  has_many :observations, through: :memberships
+  has_many :users, through: :memberships
 
   # we use this to only show existing tags on the cohort tag form. The unique
   # is important becuase otherwise we'll get duplicates of tags
@@ -22,24 +31,6 @@ class Cohort < ActiveRecord::Base
   def has_admin? user
     self.admins.include?(user)
   end
-
-  def student_memberships
-    self.memberships.where(is_admin: false)
-  end
-
-  def admin_memberships
-    self.memberships.where(is_admin: true)
-  end
-
-  def students
-    self.student_memberships.map(&:user)
-  end
-  alias_method :nonadmins, :students
-
-  def admins
-    admin_memberships.map(&:user)
-  end
-  alias_method :instructors, :admins
 
   def add_admin(user)
     self.memberships.create!(user: user, is_admin: true)
